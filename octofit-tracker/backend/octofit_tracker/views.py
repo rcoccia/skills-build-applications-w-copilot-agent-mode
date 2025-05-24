8c63ead595c24ec8c534826a84ec9653e82117c4
Rinaldo Coccia <rinaldo_coccia@hotmail.com>
2025-05-24 15:10:04 +0000

    Add API root view to return base URLs for resources

--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -1,6 +1,20 @@
+
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import User, Team, Activity, Leaderboard, Workout
 from .serializers import UserSerializer, TeamSerializer, ActivitySerializer, LeaderboardSerializer, WorkoutSerializer
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    base_url = "https://humble-waddle-xgxjxq9wgw36wvj-8000.app.github.dev/api/"
+    return Response({
+        'users': base_url + 'users/',
+        'teams': base_url + 'teams/',
+        'activity': base_url + 'activity/',
+        'leaderboard': base_url + 'leaderboard/',
+        'workouts': base_url + 'workouts/',
+    })
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
