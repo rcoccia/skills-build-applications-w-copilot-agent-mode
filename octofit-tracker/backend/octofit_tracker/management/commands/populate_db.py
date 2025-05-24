629a09b372d116da30799fe588febc5223cfa1a1
Rinaldo Coccia <rinaldo_coccia@hotmail.com>
2025-05-24 15:00:44 +0000

    find the bug

--- octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -3,7 +3,7 @@ from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
 from django.utils import timezone
 
 class Command(BaseCommand):
-    help = 'Populate the octofit_db database with test data.'
+    help = 'Populate the octofit_db database with test data for users, teams, activities, leaderboard, and workouts.'
 
     def handle(self, *args, **kwargs):
         # Users
